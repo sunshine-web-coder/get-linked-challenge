eeab95cabaf867635cc7604a311f068872dac35a
sunshine-web-coder <webcoderdev02@gmail.com>
2023-09-21 00:10:17 +0100

    second commit

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,6 @@ import { Route, Routes } from "react-router-dom";
 import "./App.scss";
 import Home from "./pages/home/Home";
 import Header from "./components/header/Header";
-import TimelinePage from "./components/TimelinePage";
 import Footer from "./components/footer/Footer";
 
 function App() {
@@ -11,7 +10,6 @@ function App() {
       <Header />
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/timeline_page" element={<TimelinePage />} />
       </Routes>
       <Footer />
     </div>
